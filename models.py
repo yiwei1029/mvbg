49aa307c874e389ea19aa4f38c9c17137f87f003
yiwei <rot1998@163.com>
2023-11-15 19:29:55 +0800

    add models and utils class

---
+++ models.py
@@ -0,0 +1,88 @@
+import numpy as np
+from numpy.linalg import multi_dot
+from utils import *
+class MVBG:
+    def __init__(self,alpha,gamma,beta):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.beta = beta
+
+    def mvbg(self,w, X, Z,d_): #d_:embedding d; X is a list of (d_v,n)
+        #step1: updating U_v
+                #(v,d_v,n)
+        v = len(w)
+        U=[multi_dot([x,Z.T, np.linalg.inv(Z.dot(Z.T))]) for x in X]
+        #step2: Updating E
+        D_F=np.diag(np.sum(axis=0))
+        D_G=np.diag(np.sum(axis=1))
+        A_temp = np.linalg.inv(np.sqrt(D_F)).dot(Z).dot(np.linalg.inv(np.sqrt(D_G))) #for SVD
+        u_A,s_A,v_A =np.linalg.svd(A_temp)
+        F = u_A[:,:d_]*np.sqrt(2)/2
+        G = v_A[:d_,:].T*np.sqrt(2)/2
+        E = np.concatenate([F,G])
+        #step3: Updating Z
+        self.update_z(w,U, X, F,G,Z)
+        #step4: Updating W
+        h = [X[i]-U[i].dot(Z) for i in range(len(w))]
+        h = [sum(h_v**2) for h_v in h]
+        power = 1/(1-self.gamma)
+        w = h**power/sum(h**power)
+
+
+    def update_z(self,w,U, X, F,G,Z):
+        F_ii = np.diagonal(dist_2m_sq(F,F))
+        G_ii = np.diagonal(dist_2m_sq(G,G))
+        #
+        F_norm = F*1/F_ii.reshape(-1,1)
+        G_norm = G*1/G_ii.reshape(-1,1)
+        d_FG = dist_2m_sq(F_norm,G_norm)
+        # U=(v,d_v,m)
+        U_temp = sum(w**self.gamma*[u.T.dot(u) for u in U]) +self.alpha*np.identity(U[0].T.shape[0])
+        V = 2*sum(w**self.gamma*[X[i].T.dot(U[i]) for i in range(len(w))])-self.beta*d_FG
+        # main step: Z=(m,n)
+        #init mu and rho
+        mu=np.random.rand()
+        rho = np.random.rand()+1
+        for i in range(Z.shape[1]):
+
+            # Fixing z_i and solving φ
+            phi = Z[:,i]+1/mu*(eta-U_temp.T.dot(Z[:,i]))
+            #Fixing φ and solving zi
+            k = 1/mu*(mu*phi-eta-U_temp.dot(phi)+V[i,:].T)
+            #find lmda
+            for lmda in np.linspace(-np.abs(max(k)),np.abs(max(k)),10000):
+                z_i = np.where(k+lmda>=0,k+lmda,0)
+                if(np.abs(z_i.sum())<1e-3):
+                    break
+            
+            eta = eta+mu*(z_i-phi)
+            mu = rho*mu
+
+            Z[:,i]=z_i #
+
+class PCA:
+    def cpca(self,X,d_): #X: list of (d_v,n)
+        X = np.concatenate(X)
+        X = X-X.mean(axis=1).reshape(-1,1)
+        cov_X = X.dot(X.T)
+        eigenval,eigenvec = np.linalg.eig(cov_X)
+        eigenval = np.real(eigenval)
+        eigenvec = np.real(eigenvec)    
+        idx = np.argpartition(eigenval,-d_)[-d_:]
+        select_vec = eigenvec[:,idx]
+        out = select_vec.T.dot(X)
+        return out
+
+    def dpca(self,X,d_):
+        dim_reductions =[]
+        for x in X:
+            x = x-x.mean(axis=1).reshape(-1,1)
+            cov_x = x.dot(x.T)
+            eigenval,eigenvec = np.linalg.eig(cov_x)
+            eigenval = np.real(eigenval)
+            eigenvec = np.real(eigenvec)    
+            idx = np.argpartition(eigenval,-d_)[-d_:]
+            select_vec = eigenvec[:,idx]
+            out = select_vec.T.dot(x)
+            dim_reductions.append(out)
+        return sum(dim_reductions)/len(dim_reductions)
--- utils.py
+++ utils.py
@@ -1,63 +1,18 @@
-# %%
 import numpy as np
-def cpca(X,d_): #X: list of (d_v,n)
-    X = np.concatenate(X)
-    X = X-X.mean(axis=1).reshape(-1,1)
-    cov_X = X.dot(X.T)
-    eigenval,eigenvec = np.linalg.eig(cov_X)    
-    select_vec = eigenvec[:,np.partition(eigenval,-d_)[-d_:]]
-    out = select_vec.T.dot(X)
-    return out
+def dist_2m_sq(A,B):
+    #assume A=(n,d_),B=(m,d_),expand A=>(n,m,d_),B=>(n,m,d_)
+    n = A.shape[0]
+    m = B.shape[0]
+    # expand
+    A_tile = np.tile(np.expand_dims(A,1),[1,m,1])
+    B_tile = np.tile(np.expand_dims(B,0),[n,1,1])
+    dist = np.sum(np.square(A_tile-B_tile),axis=-1)
+    return dist
 
-
-
-
-
-
-
-# %%
-import tarfile
-import os
-from tqdm import tqdm
-import numpy as np
-def untar(fname, dirs):
-    """
-    解压tar.gz文件
-    :param fname: 压缩文件名
-    :param dirs: 解压后的存放路径
-    :return: bool
-    """
-    try:
-        t = tarfile.open(fname)
-        t.extractall(path = dirs)
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-# untar('datasets/msrcorid.tar.gz','./datasets/')
-from PIL import Image
-def read_imgs():
-    path = 'datasets/msrcorid'
-    filepaths = []
-    img_arrays = []
-    labels = []
-    for root, dirs, files in tqdm(os.walk(path)):
-        # print(dirnames)     
-        for file in files:
-            if(file.endswith('JPG')):                         # 遍历文件
-                file_path = os.path.join(root, file)
-                img = np.asarray(Image.open(file_path))
-                img_arrays.append(img.transpose((2,0,1)).reshape((3,-1,1)))
-                labels.append(file[:3])   # 获取文件绝对路径  
-                filepaths.append(file_path)            # 将文件路径添加进列表
-        # for dir in dirs:                           # 遍历目录下的子目录
-        #     dir_path = os.path.join(root, dir)     # 获取子目录路径
-        #     all_files_path(dir_path)               # 递归调用    
-    return img_arrays,labels
-
-img_arrays,labels = read_imgs()
-img_tensor = np.concatenate(img_arrays[:200],axis =2)
-img_tensor.shape 
-
-
+import scipy.io as sio
+from sklearn.cluster import KMeans
+def read_data(filename):
+    data = sio.loadmat(filename)
+    X = data['X']
+    labels = data['Y']
+    return X,labels
