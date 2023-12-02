5c8dfd735f29faffccb44096bbb26b2fff0b15d7
yiwei1029 <rot1998@163.com>
2023-12-02 18:46:33 +0800

    debug

--- main_1 (2).py
+++ main_1 (2).py
@@ -15,7 +15,7 @@ def main_cal(model_name,params,dt_name):
     # data
     X,labels=read_data(f'datasets/mats/{dt_name}.mat')
     X = [X[0,i].T for  i  in range(X.shape[1])]
-    X =  [x[:300,:700] for x in X] #d<=300 ,n = X[0].shape[1]
+    X =  [x[:300,:500] for x in X] #d<=300 ,n = X[0].shape[1]
     if isinstance(X[0],scipy.sparse._csr.csr_matrix):
         X =   [np.asarray(x.todense()) for x in X]
     labels  = labels.squeeze()
@@ -23,12 +23,14 @@ def main_cal(model_name,params,dt_name):
     #models
     #main loop
     res_nmi = [];res_acc =[];res_ari=[];res_purity  = []
-    train_ratio=0.1
-    d_max  =  min(math.floor((1-train_ratio)*X[0].shape[1]),int(np.min([x.shape[0] for x in X])) ,100) #min(d,測試x的維數)
-    d_range  = range(k,d_max,3)
+    train_ratio=0.5
+    # d_max  =  min(math.floor((1-train_ratio)*X[0].shape[1]),\
+    #               int(np.min([x.shape[0] for x in X])) ,100) #min(d,测试x的维数)
+    d_range  = range(2,30,2)
     model  =  model_name
+    
 
-    for d_ in tqdm(d_range): 
+    for d_ in tqdm(d_range):
         nmi_list = [];acc_list =[]; ari_list=[]; purity_list=[]
         for i in range(5):
             # train_test split
@@ -36,22 +38,11 @@ def main_cal(model_name,params,dt_name):
             X_train = [x[:,train_idx] for x in X]
             X_test = [x[:,test_idx] for x in X]
             y_test = labels[test_idx]
-            #train
-
-            # pred =  model.predict(X_test,0.5,2,1e6,d_,k,10) #MVP ok
-            # pred   = model.predict(X_train,X_test,1e7,d_,k,200) #lPP ok 
-            # pred  = model.predict(X_test,d_,20,k) #LE ok
-            # pred = model.predict(X_train,X_test,d_,k) #DPCA ok
-            # pred =  model.predict(X_test,0.5,d_,1e8,k,100) #MSE ok
-            # pred   =  model.predict(X_test,d_,1e8,5,100,100)#mdcr ok
-            # pred = model.predict(X_test,d_max,d_,k,1e8,30)# m: >=dmax ok mvbg
-            # pred = model.dse(X_test,k,2,100) #dse 
-            # pred  = model.predict(X_train, X_test,d_,k) #CPCA
+            #train and predict
             pred = eval(model_name+'.predict'+params)
             # criterion
-            #nmi
             from sklearn.metrics.cluster import normalized_mutual_info_score,adjusted_rand_score
-
+            #nmi
             nmi = normalized_mutual_info_score(y_test,pred)
             nmi_list.append(nmi)
             #acc
@@ -64,24 +55,23 @@ def main_cal(model_name,params,dt_name):
             purity  =  purity_score(y_test,pred)
             purity_list.append(purity)
         #
-        nmi = np.mean(nmi_list)
-        acc  = np.mean(acc_list)
-        ari = np.mean(ari_list)
-        purity = np.mean(purity_list)
+        nmi_ = np.mean(nmi_list)
+        acc_  = np.mean(acc_list)
+        ari_ = np.mean(ari_list)
+        purity_ = np.mean(purity_list)
         # 
-        res_nmi.append(nmi)
-        res_acc.append(acc)
-        res_ari.append(ari)
-        res_purity.append(purity)
+        res_nmi.append(nmi_)
+        res_acc.append(acc_)
+        res_ari.append(ari_)
+        res_purity.append(purity_)
         # print('./result/nmi/{}/{}.npy'.format(dt_name, eval(model+'.name')))
     for indicator in ['nmi','acc','ari','purity']:
-        if not (os.path.exists('./result/{}/{}/'.format(indicator,dt_name))):
-            os.makedirs('./result/{}/{}/'.format(indicator,dt_name))
-        np.save('./result/{}/{}/{}.npy'.format(indicator,dt_name, eval(model+'.name')),eval('res_'+indicator))
-    # np.save('./result/nmi/{}/{}.npy'.format(dt_name, eval(model+'.name')),res_nmi)
-    # np.save('./result/acc/{}/{}.npy'.format(dt_name, eval(model+'.name')),res_acc)
-    # np.save('./result/ari/{}/{}.npy'.format(dt_name, eval(model+'.name')),res_ari)
-    # np.save('./result/purity/{}/{}.npy'.format(dt_name, (eval(model+'.name'))),res_purity)
+        if not (os.path.exists('./result1/{}/{}/'.format(indicator,dt_name))):
+            os.makedirs('./result1/{}/{}/'.format(indicator,dt_name))
+        np.save('./result1/{}/{}/{}.npy'.format(indicator,dt_name, \
+                                               eval(model+'.name')),eval('res_'+indicator))
+        np.save('./result1/{}/{}/{}.npy'.format(indicator,dt_name,'d_range'),np.array(d_range))
+        
 def err_call_back(err):
         print(f'error：{str(err)} ')
 import traceback
@@ -108,18 +98,20 @@ class LogExceptions(object):
         return result
     pass
 if __name__=='__main__':
-    model_params_dict={'MSE()':'(X_test,0.5,d_,1e8,k,100)',
-                        'MDcR()':'(X_test,d_,1e8,5,100,100)',
-                        'DSE()':'(X_test,k,2,100)',
-                        'MVBG(0.1,2,0.1)':'(X_test,d_max,d_,k,1e8,30)',
+    model_params_dict={'MSE()':'(X_test,0.5,d_,1e8,k,10)',
+                        'MDcR()':'(X_test,d_,1e8,5,k,10)',
+                        'DSE()':'(X_test,k,2,10)',
+                        'MVBG(0.1,2,0.1)':'(X_test,30,d_,k,1e8,10)',
                         'CPCA()':'(X_train, X_test,d_,k)',
                         'DPCA()':'(X_train,X_test,d_,k)',
                         'MVP()':'(X_test,0.5,2,1e6,d_,k,10)',
-                        'LPP()':'(X_train,X_test,1e7,d_,k,200)',
+                        'LPP()':'(X_train,X_test,1e7,d_,k,20)',
                         'LE()':'(X_test,d_,20,k)'}
     datasets_names = ['BBC','MSRC-v1','NGs','Reuters','YALE']
+    datasets_names = ['Reuters']
+
     multiprocessing.log_to_stderr()  # 加上此行
-    p = Pool(50)
+    p = Pool(2)
     
     for model,params in model_params_dict.items():
         for dt_name in datasets_names:
